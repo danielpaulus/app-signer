28c5a1c1545c72fa4760e9f5a6e802c383762c78
Daniel Paulus <daniel.paulus@icloud.com>
2021-12-11 22:24:38 +0100

    add docopt

--- go.mod
+++ go.mod
@@ -16,5 +16,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,33 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"github.com/danielpaulus/app-signer/api"
 	"github.com/danielpaulus/app-signer/architecturecheck"
+	"github.com/docopt/docopt-go"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 )
 
 func main() {
+	version := "1.0"
+	usage := fmt.Sprintf(`sign %s
 
+Usage:
+  sign --udid=<udid> --p12password=<p12password> --profilespath=<profilespath> --ipa=<ipa> --output=<output> [options]
+
+Options:
+  -v --verbose   Enable Debug Logging.
+  -t --trace     Enable Trace Logging (dump every message).
+  --nojson       Disable JSON output (default).
+  -h --help      Show this screen.
+
+The commands work as following:
+  `, version)
+	arguments, err := docopt.ParseDoc(usage)
 	log.WithFields(log.Fields{"args": os.Args}).Infof("starting iOS appsigner")
+	udid, _ := arguments.String("--udid")
+	profilePassword, _ := arguments.String("--p12password")
+	profilespath, _ := arguments.String("--profilespath")
+	outputFileName, _ := arguments.String("--output")
+	ipaFile, _ := arguments.String("--ipa")
 
-	profilePassword := "a"
-	profilesDir := "provisioningprofiles"
-	ipaFile := "/Users/danielpaulus/privaterepos/app-signer/codesign/fixtures/wda.ipa"
-	udid := "00008101-000A402A0EBA001E"
-	outputFileName :="resigned.ipa"
-	err := architecturecheck.CheckLipo()
+	err = architecturecheck.CheckLipo()
 	if err != nil {
 		log.WithFields(log.Fields{"err": err}).Error("lipo is not installed, make sure xcode is installed or add lipo to /usr/bin")
 		return
 	}
 	workdir, err := ioutil.TempDir("", "pattern")
 	defer os.RemoveAll(workdir)
-	s, err := api.PrepareSigningWorkspace(workdir, profilePassword, profilesDir)
+	s, err := api.PrepareSigningWorkspace(workdir, profilePassword, profilespath)
 	defer s.Close()
-	_,err = api.ResignIPA(s, udid, ipaFile, outputFileName)
-	if err!=nil{
+	_, err = api.ResignIPA(s, udid, ipaFile, outputFileName)
+	if err != nil {
 		log.Error(err)
 		return
 	}
