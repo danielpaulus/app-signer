8b0cde7fa6b6dbecb96beb52e01782a11e9a62ad
Yashar Bagher azari <20423111+YasharAzari@users.noreply.github.com>
2022-02-03 16:00:53 +0100

    feature parity with the previous cli cmd

---
+++ cmd/sign.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/danielpaulus/app-signer/api"
+	"github.com/danielpaulus/app-signer/architecturecheck"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+type SigningInputs struct {
+	ReferenceUdid              string
+	ProfileCertificatePassword string
+	ProfilesPath               string
+	OutputFileName             string
+	IpaFileToSign              string
+}
+
+var signingInputs = SigningInputs{}
+
+var signCmd = &cobra.Command{
+	Use:   "sign",
+	Short: "Sign your apps or ipa using a list of mobile provision, and without a reference udid",
+	Long: `By providing your .p12 certificate as well as a list of mobileprovision files, 
+you will be able to sign your app or ipa with every mobileprovision available in the profiles path`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := sign(); err != nil {
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	enableBaseSigningRequirements(signCmd, false)
+}
+
+func enableBaseSigningRequirements(cmd *cobra.Command, udidRequired bool) {
+	cmd.Flags().StringVar(&signingInputs.ReferenceUdid, "udid", "", "Reference udid in order to select the provisioning profile located in your profiles folder")
+	cmd.Flags().StringVar(&signingInputs.ProfileCertificatePassword, "p12password", "", "Password for your .p12 located in the your profiles folder")
+	cmd.Flags().StringVar(&signingInputs.ProfilesPath, "profilespath", "", "Path to your profiles folder. It should contains a list of mobileprovision as well as a single .p12 associated with them")
+	cmd.Flags().StringVar(&signingInputs.OutputFileName, "output", "", "Output path for the signed app or ipa")
+	cmd.Flags().StringVar(&signingInputs.IpaFileToSign, "ipa", "", "Path to the target ipa to be signed")
+	if udidRequired {
+		cmd.MarkFlagRequired("udid")
+	}
+	cmd.MarkFlagRequired("p12password")
+	cmd.MarkFlagRequired("profilespath")
+	cmd.MarkFlagRequired("output")
+	cmd.MarkFlagRequired("ipa")
+}
+
+func sign() error {
+	log.Trace("popo")
+	err := architecturecheck.CheckLipo()
+	if err != nil {
+		log.WithFields(log.Fields{"err": err}).Error("lipo is not installed, make sure xcode is installed or add lipo to /usr/bin")
+		return err
+	}
+	workdir, err := ioutil.TempDir("", "pattern")
+	defer os.RemoveAll(workdir)
+	s, err := api.PrepareSigningWorkspace(workdir, signingInputs.ProfileCertificatePassword, signingInputs.ProfilesPath)
+	defer s.Close()
+	_, err = api.ResignIPA(s, signingInputs.ReferenceUdid, signingInputs.IpaFileToSign, signingInputs.OutputFileName)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	log.Infof("resigned:")
+	return nil
+}
--- main.go
+++ main.go
@@ -1,51 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/danielpaulus/app-signer/api"
-	"github.com/danielpaulus/app-signer/architecturecheck"
-	"github.com/docopt/docopt-go"
-	log "github.com/sirupsen/logrus"
-	"io/ioutil"
-	"os"
-)
+import "github.com/danielpaulus/app-signer/cmd"
 
 func main() {
-	version := "1.0"
-	usage := fmt.Sprintf(`sign %s
-
-Usage:
-  sign --udid=<udid> --p12password=<p12password> --profilespath=<profilespath> --ipa=<ipa> --output=<output> [options]
-
-Options:
-  -v --verbose   Enable Debug Logging.
-  -t --trace     Enable Trace Logging (dump every message).
-  --nojson       Disable JSON output (default).
-  -h --help      Show this screen.
-
-The commands work as following:
-  `, version)
-	arguments, err := docopt.ParseDoc(usage)
-	log.WithFields(log.Fields{"args": os.Args}).Infof("starting iOS appsigner")
-	udid, _ := arguments.String("--udid")
-	profilePassword, _ := arguments.String("--p12password")
-	profilespath, _ := arguments.String("--profilespath")
-	outputFileName, _ := arguments.String("--output")
-	ipaFile, _ := arguments.String("--ipa")
-
-	err = architecturecheck.CheckLipo()
-	if err != nil {
-		log.WithFields(log.Fields{"err": err}).Error("lipo is not installed, make sure xcode is installed or add lipo to /usr/bin")
-		return
-	}
-	workdir, err := ioutil.TempDir("", "pattern")
-	defer os.RemoveAll(workdir)
-	s, err := api.PrepareSigningWorkspace(workdir, profilePassword, profilespath)
-	defer s.Close()
-	_, err = api.ResignIPA(s, udid, ipaFile, outputFileName)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	log.Infof("resigned:")
+	cmd.Execute()
 }
